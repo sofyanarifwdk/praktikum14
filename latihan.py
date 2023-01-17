faf86740a2c86248f9c7d0c3b8c2dfbc9e4f17d4
sofyan.dev <arifwdk@gmail.com>
2023-01-17 19:13:57 +0700

    praktikum14

---
+++ latihan.py
@@ -0,0 +1,17 @@
+# Latihan 1
+txt = 'Hello World'
+print('variabel txt : '+ txt)
+
+print("hitung jumlah karakter : " + str(len(txt)))
+
+print("ambil karakter terakhir : " + txt[len(txt)-1])
+
+print('ambil karakter terakhir : ' + txt[2:5])
+
+print('hilangkan spasi pada Hello World : ' + txt.replace(" ", ''))
+
+print('ubah text jadi huruf besar : ' + txt.upper())
+
+print('ubah text jadi huruf kecil : ' + txt.lower())
+
+print('ganti karakter H jadi J : ' + txt.replace('H', 'J'))
---
+++ latihan2.py
@@ -0,0 +1,3 @@
+umur = 24
+txt = "Hello, Nama saya John, dan umur saya adalah {0} tahun"
+print(txt.format(umur))
